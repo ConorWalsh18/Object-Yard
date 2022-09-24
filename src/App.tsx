6f159aa9a88397271235b3033ea456aa9b5059f8
ConorWalsh18 <conor.walsh@comcast.net>
2022-09-24 15:57:20 -0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Grid, Button, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 function App() {
+  const [showContent, setShowContent] = React.useState(false);
+  const [contentToShow, setContentToShow] = React.useState("home");
+
+  //TODO: Add a hover effect for the button
+  const StyledButton = styled(Button)({
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 25,
+    color: "black",
+    transition: "all 0.25s",
+    "&:hover": {
+      backgroundColor: "transparent",
+      borderColor: "transparent",
+      boxShadow: "none",
+      paddingBottom: "20px",
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <div className="header-bar" style={{minHeight: showContent ? "10vh" : "20vh"}}>
+        <StyledButton variant="text" disableRipple={true}>Object.Yard</StyledButton>
+        <StyledButton variant="text" disableRipple={true}>Info</StyledButton>
+      </div>
+      <Grid container spacing={2} direction="column" justifyContent="center" alignItems="center" sx={{width: "80vw"}}>
+        <Grid item xs={12}>
+          <div className="content-container">
+            <StyledButton
+              variant="text"
+              disableRipple={true}
+              onClick={() => {setShowContent(!showContent)}}>loitering with intent</StyledButton>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="content-container">
+            <StyledButton variant="text" disableRipple={true}>phatjazz</StyledButton>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="content-container">
+            <StyledButton variant="text" disableRipple={true}>praying</StyledButton>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="content-container">
+            <StyledButton variant="text" disableRipple={true}>forced auction</StyledButton>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="content-container">
+            <StyledButton variant="text" disableRipple={true}>dew drops</StyledButton>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="content-container">
+            <StyledButton variant="text" disableRipple={true}>you dropped this</StyledButton>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/loitering-with-intent.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import { Grid, Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+function LoiteringWithIntent() {
+  return (
+    <div className="page-container">
+      <Grid container spacing={2} direction="column" justifyContent="center" alignItems="center" sx={{width: "80vw"}}>
+        
+      </Grid>
+    </div>
+  );
+}
+
+export default LoiteringWithIntent;
